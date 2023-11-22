4cfd27d2ac910b43345d8f7b6f70368aa30f9ca3
dominikberger01 <dominik@mydigital.solutions>
2023-11-22 22:09:22 +0100

    add lambda configuration

--- main.tf
+++ main.tf
@@ -8,7 +8,42 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-central-1"
+  region     = "eu-central-1"
   access_key = var.aws_key
   secret_key = var.aws_secret
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "main.py"
+  output_path = "lambda_main.py.zip"
+}
+
+resource "aws_lambda_function" "hello_world" {
+  filename      = "lambda_main.py.zip"
+  function_name = "main"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "main.hello_world"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "python3.11"
 }
