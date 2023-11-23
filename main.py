f18d884cbeb624f9a3e5c0f9e5ebda6e93306224
dominikberger01 <dominik@mydigital.solutions>
2023-11-24 00:22:24 +0100

    fix unexpected function response bug

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ def hello_world(event, context):
     print(f'Event: {event} \n')
     print(f'Context: {context}')
 
-    return json.dumps({
+    return {
         'statusCode': 200,
         'headers': {
             'Content-Type': 'application/json',
         },
-        'body': {'message': 'Hello World!'}
-    })
+        'body': json.dumps({'message': 'Hello World!'})
+    }
